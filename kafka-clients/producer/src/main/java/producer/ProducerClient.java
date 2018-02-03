cf779c75c80da0f1adcbbc75eb116c9bf3d97dc8
Priyam Mitra <pmitra43@gmail.com>
2018-02-03 18:09:28 +0530

    refactoring

--- kafka-clients/producer/src/main/java/producer/ProducerClient.java
+++ kafka-clients/producer/src/main/java/producer/ProducerClient.java
@@ -1,30 +1,47 @@
 package producer;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class ProducerClient {
-	public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(ProducerClient.class);
 
-		//Setup KafkaConfig properties
+    private static final String TOPIC = "my-topic";
+    private static final String BOOTSTRAP_SERVERS = "kafka-cluster:9092";
 
-		Properties properties = new Properties();
-		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		properties.put(ProducerConfig.ACKS_CONFIG, "all");
-		properties.put(ProducerConfig.RETRIES_CONFIG, 0);
-		properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+    public static void main(String[] args) {
+        final Producer<Integer, String> producer = createProducer();
+        // Create Producer
 
-		// Create Producer
-		try (Producer<String, String> producer = new KafkaProducer<String, String>(properties)) {
-			for (int i = 0; i < 100; i++) {
-				producer.send(new ProducerRecord<>("my-topic", Integer.toString(i), Integer.toString(i)));
-			}
-		}
-	}
+        for (int i = 0; i < 100; i++) {
+            ProducerRecord<Integer, String> record =
+                    new ProducerRecord<>(TOPIC, i, "message count: " + i);
+
+            try {
+                RecordMetadata metadata = producer.send(record).get();
+                logger.info("Message sent: record={}, metadata={}", record, metadata);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
+
+        producer.close();
+    }
+
+    private static Producer<Integer, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                IntegerSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
 }
