213a9f1db635c0baca6801ff63f701b8007103ba
Priyam Mitra <pmitra43@gmail.com>
2018-02-03 17:36:52 +0530

    created basic consumer

--- kafka-clients/consumer/src/main/java/consumer/Consumer.java
+++
@@ -1,4 +0,0 @@
-package consumer;
-
-public class Consumer {
-}
--- kafka-clients/consumer/src/main/java/consumer/ConsumerClient.java
+++ kafka-clients/consumer/src/main/java/consumer/ConsumerClient.java
@@ -1,16 +1,55 @@
-package producer;
+package consumer;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.Properties;
 
 public class ConsumerClient {
+
+	private static final Logger logger = LoggerFactory.getLogger(ConsumerClient.class);
+
+	private static final String TOPIC = "my-topic";
+	private static final String BOOTSTRAP_SERVERS = "kafka-cluster:9092";
+
 	public static void main(String[] args) {
+		Consumer<Long, String> consumer = createConsumer();
+		int limit = 100;
+		int emptyResponse = 0;
+		while (true) {
+			final ConsumerRecords<Long, String> records = consumer.poll(1000);
 
-		//Setup KafkaConfig properties
+			if (records.isEmpty()) {
+				emptyResponse += 1;
+				if (emptyResponse > limit)
+					break;
+			}
 
+			records.forEach(record -> logger.info("Message received: {}", record));
+
+			consumer.commitSync();
+		}
+		consumer.close();
+
+	}
+
+	private static Consumer<Long, String> createConsumer() {
+		final Properties props = new Properties();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+		final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+
+		consumer.subscribe(Collections.singletonList(TOPIC));
+		return consumer;
 	}
 }
